14b6f7b1caddb7803f98746550bcd38b946b6f70
selvester69 <selvester69@gmail.com>
2021-06-28 23:47:09 +0530

    added member login and signup page

---
+++ frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa13btombI8gcbS85d7',
+        issuer: 'https://dev-90705698.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
